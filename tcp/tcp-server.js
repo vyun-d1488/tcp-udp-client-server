60f16258474921919d63e7548ef3509223a5a3c9
vyun-d1488 <ilyaspiypiy@gmail.com>
2020-06-25 12:42:47 +0600

    tcp init

---
+++ tcp/tcp-server.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const net = require("net");
+
+net.createServer((socket) => {
+      console.dir(socket.address());
+      socket.setNoDelay(true);
+      socket.write("💗");
+      socket.on("data", (data) => {
+            console.log("📨:", data);
+      });
+}).listen(2000);
